24b121b52bc04dbc54123c0ffa987c693eaf5029
Erza Akbar <akbarerza3@gmail.com>
2026-03-16 17:09:31 +0700

    update

--- projects/02-api-testing/helpers/api-helper.js
+++ projects/02-api-testing/helpers/api-helper.js
@@ -2,59 +2,114 @@
 class ApiHelper {
   constructor(request) {
     this.request = request;
+    this.baseUrl = 'https://jsonplaceholder.typicode.com';  // <-- TAMBAHKAN DEFAULT BASE URL
   }
 
   // GET request
   async get(endpoint, params = {}) {
-    const response = await this.request.get(endpoint, {
-      params: params
-    });
+    // Gabungkan baseUrl dengan endpoint
+    const url = new URL(endpoint, this.baseUrl);  // Sekarang this.baseUrl ada nilainya
+    
+    // Tambahkan query parameters
+    Object.keys(params).forEach(key => 
+      url.searchParams.append(key, params[key])
+    );
+
+    const response = await this.request.get(url.toString());
+    let responseBody;
+
+    try {
+      responseBody = await response.json();
+    } catch (error) {
+      responseBody = await response.text();
+    }
+
     return {
       status: response.status(),
-      body: await response.json(),
+      body: responseBody,
       headers: response.headers()
     };
   }
 
   // POST request
   async post(endpoint, data = {}) {
-    const response = await this.request.post(endpoint, {
-      data: data
+    const url = new URL(endpoint, this.baseUrl);
+    const response = await this.request.post(url.toString(), { 
+      data: data,
+      headers: {
+        'Content-Type': 'application/json'
+      }
     });
+    
+    let responseBody;
+    try {
+      responseBody = await response.json();
+    } catch (error) {
+      responseBody = await response.text();
+    }
+
     return {
       status: response.status(),
-      body: await response.json(),
+      body: responseBody,
       headers: response.headers()
     };
   }
 
   // PUT request
   async put(endpoint, data = {}) {
-    const response = await this.request.put(endpoint, {
-      data: data
+    const url = new URL(endpoint, this.baseUrl);
+    const response = await this.request.put(url.toString(), { 
+      data: data,
+      headers: {
+        'Content-Type': 'application/json'
+      }
     });
+    
+    let responseBody;
+    try {
+      responseBody = await response.json();
+    } catch (error) {
+      responseBody = await response.text();
+    }
+
     return {
       status: response.status(),
-      body: await response.json()
+      body: responseBody,
+      headers: response.headers()
     };
   }
 
   // DELETE request
   async delete(endpoint) {
-    const response = await this.request.delete(endpoint);
+    const url = new URL(endpoint, this.baseUrl);
+    const response = await this.request.delete(url.toString());
     return {
-      status: response.status()
+      status: response.status(),
+      headers: response.headers()
     };
   }
 
   // PATCH request
   async patch(endpoint, data = {}) {
-    const response = await this.request.patch(endpoint, {
-      data: data
+    const url = new URL(endpoint, this.baseUrl);
+    const response = await this.request.patch(url.toString(), { 
+      data: data,
+      headers: {
+        'Content-Type': 'application/json'
+      }
     });
+    
+    let responseBody;
+    try {
+      responseBody = await response.json();
+    } catch (error) {
+      responseBody = await response.text();
+    }
+
     return {
       status: response.status(),
-      body: await response.json()
+      body: responseBody,
+      headers: response.headers()
     };
   }
 }
